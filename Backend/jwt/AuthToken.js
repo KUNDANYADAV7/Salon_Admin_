2daaaa7401c64abeda3a16f1d6f5a9de2fc3d0bf
KUNDANYADAV7 <kdyd999@gmail.com>
2025-04-03 17:09:12 +0530

    jwt-auth

--- Backend/jwt/AuthToken.js
+++ Backend/jwt/AuthToken.js
@@ -18,6 +18,9 @@
 // export default createTokenAndSaveCookies;
 
 
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
 const createTokenAndSaveCookies = async (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
     expiresIn: "30d",
