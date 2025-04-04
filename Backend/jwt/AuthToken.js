62d05dc51c05171df61379bb3b25d3f84ca2bf43
KUNDANYADAV7 <kdyd999@gmail.com>
2025-04-04 10:12:23 +0530

    jwt

--- Backend/jwt/AuthToken.js
+++ Backend/jwt/AuthToken.js
@@ -1,3 +1,22 @@
+// import jwt from "jsonwebtoken";
+// import { User } from "../models/user.model.js";
+
+// const createTokenAndSaveCookies = async (userId, res) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+//     expiresIn: "30d",
+//   });
+//   res.cookie("jwt", token, {
+//     httpOnly: true, // Temporarily set to false for testing
+//     secure: false,
+//     sameSite: "lax",
+//     path: "/", // Ensure the cookie is available throughout the site
+//   });
+//   await User.findByIdAndUpdate(userId, { token });
+//   return token;
+// };
+
+// export default createTokenAndSaveCookies;
+
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
@@ -6,9 +25,9 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
-    secure: false,
-    sameSite: "lax",
+    httpOnly: true,
+  secure: true,  // Set to true if using HTTPS
+  sameSite: "None", // Required for cross-origin cookies
     path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
